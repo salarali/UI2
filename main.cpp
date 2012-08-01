9cce0c3f0d4a7a8678276790d60a97976682cbf5
Antonio Nava <anton.diaz.nava@gmail.com>
2012-08-01 00:10:39 -0400

    Added appropriate main

--- main.cpp
+++ main.cpp
@@ -1,10 +1,30 @@
-#include "qtest3.h"
-#include <QtGui/QApplication>
+#include "simulator.h"
+#include "MyGraphicsScene.h"
+#include "MyGraphicsView.h"
+#include <qtimeline.h>
+#include <time.h>
+#include <qapplication.h>
+#include <qgraphicsview.h>
+
 
 int main(int argc, char *argv[])
 {
-	QApplication a(argc, argv);
-	qtest3 w;
-	w.show();
-	return a.exec();
+    QApplication app(argc, argv);
+
+    qsrand(time(0));
+
+    MyGraphicsScene scene;
+    scene.setItemIndexMethod(QGraphicsScene::NoIndex);
+    scene.setBackgroundBrush(Qt::white);
+    scene.setSceneRect(0, 0, 740, 540);
+
+    MyGraphicsView view(&scene);
+    view.setViewportUpdateMode(QGraphicsView::FullViewportUpdate);
+    view.setRenderHint(QPainter::Antialiasing);
+    view.show();
+
+    Simulator simulator(&scene);
+    simulator.start();
+
+    return app.exec();
 }
